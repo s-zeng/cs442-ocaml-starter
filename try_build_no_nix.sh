be14154dde39eae959abe107c4b9bff00135576d
Simon Zeng <contact@simonzeng.com>
2023-01-25 11:04:59 -0500

    add something for the non nix people

---
+++ try_build_no_nix.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+ocamlc --version
+mkdir -p ./tmp
+cp ./src/$1.ml ./tmp
+ocamlfind ocamlc -package core,stdio -linkpkg -thread -o ./tmp/$1 ./tmp/$1.ml
+./tmp/$1
+echo "done"
+rm -rf ./tmp
